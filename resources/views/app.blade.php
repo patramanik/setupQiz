100fe9d64fef677eadfa4a58ef724ec439c27ed5
kaushik <kaushikpaul29999@gmail.com>
2026-03-22 21:44:53 +0530

    add tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1
+        class="text-4xl md:text-5xl font-extrabold text-center text-white 
+           bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 
+           py-4 px-6 rounded-2xl shadow-lg 
+           hover:scale-105 transform transition duration-300 
+           underline decoration-yellow-300 decoration-4">
+        Hello world!
+    </h1>
+</body>
+
+</html>
